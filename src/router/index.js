07432e364b72580aa475b7f5495b3dd44f79def4
Ilham Muttaqien <imuttaqien17@gmail.com>
2022-11-28 00:04:00 +0700

    Add route for handling unknown route
    Signed-off-by: Ilham Muttaqien <imuttaqien17@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 const Home = { template: '<div>Home</div>' };
 import Login from '@/views/Login.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   {
@@ -12,6 +13,10 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: NotFound,
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Page Not Found!</h2>
+</template>
+<script>
+export default {
+  name: "NotFound" 
+}
+</script>
