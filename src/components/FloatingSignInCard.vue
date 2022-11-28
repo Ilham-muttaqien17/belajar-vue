d997808ae86ff247c4aceebf86b6950bb52a465f
Ilham Muttaqien <imuttaqien17@gmail.com>
2022-11-28 14:20:09 +0700

    Add floating sign up card
    Signed-off-by: Ilham Muttaqien <imuttaqien17@gmail.com>

---
+++ src/components/FloatingSignInCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="absolute top-[40%] -left-20 w-[350px] bg-black bg-opacity-50 rounded-xl px-16 py-10 border border-orange-400">
+    <h2 class="text-4xl text-white font-[700] mb-10">Sign In</h2>
+    <p class="text-white font-[700]">Sign In to unlock Exciting experience with our services</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "FloatingSignIn"
+}
+</script>
--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="bg-dark-blue w-[400px] lg:w-[900px] max-w-[1200px] h-[650px] max-h-[800px] rounded-3xl grid grid-cols-1 lg:grid-cols-2 p-2">
+  <div class="relative bg-dark-blue w-[400px] lg:w-[900px] max-w-[1200px] h-[650px] max-h-[800px] rounded-3xl grid grid-cols-1 lg:grid-cols-2 p-2">
     <div class="bg-no-repeat bg-cover bg-center rounded-l-3xl w-full h-full image-card hidden lg:block"></div>
     <login-form></login-form>
+    <floating-sign-in></floating-sign-in>
   </div>
 </template>
 <script>
 import LoginForm from './LoginForm.vue'
+import FloatingSignIn from './FloatingSignInCard.vue'
 
 export default {
   name: "LoginCard",
   components: {
-    LoginForm
+    LoginForm,
+    FloatingSignIn
   }
 }
 </script>
